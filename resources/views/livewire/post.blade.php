c0f07e7eac3176748d10ce157c5a8b625d4d832f
heryfik <hery.fik@gmail.com>
2023-01-05 15:50:33 +0700

    tes1

--- resources/views/livewire/post.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <div class="max-w-4xl mx-auto py-20 prose lg:prose-xl">
-        <h1 class="truncate">{{ $post->title }}</h1>
-        <p class="overflow-ellipsis overflow-hidden">{{ $post->body }}</p>    
-    </div>
-</div>
